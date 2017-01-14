9b086e6f100a36b34a2d98dedaeb0290479d6a54
mbmain2013 <mbmain2013@gmail.com>
2017-01-15 00:13:19 +0100

    Tabfragment extended to accept Activity with an Initialize method

--- app/src/main/java/com/example/mb7/sportappbp/TabFragment.java
+++ app/src/main/java/com/example/mb7/sportappbp/TabFragment.java
@@ -1,15 +1,22 @@
 package com.example.mb7.sportappbp;
 
 
-        import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.support.v4.app.Fragment;
 /**
  * Created by MB7 on 08.01.2017.
  */
 
 public class TabFragment extends Fragment {
 
+
         private String title;
+        private Activity activity;
         String getTitle(){return title;}
         void setTitle(String title1) { title = title1;}
-
+        void Initialize(Activity act, String t)
+        {
+                activity = act;
+                title = t;
+        }
 }

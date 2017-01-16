99ae47e2a0d1edc0341c71e95146a36d0785ef68
Basti <sjestaedt@googlemail.com>
2017-01-16 20:04:56 +0100

    Create diary activity -create the layout ->insert the listview -create the class ->implement the listview with an Sample of Strings

---
+++ app/src/main/java/com/example/mb7/sportappbp/DiaryActivity.java
@@ -0,0 +1,25 @@
+package com.example.mb7.sportappbp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DiaryActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> arrayAdapter;
+    String[] entries =  {"1.1.2017","2.1.2017","3.1.2017","4.1.2017","5.1.2017"};
+    ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+
+        listView = (ListView) findViewById(R.id.listviewDiary);
+        arrayAdapter = new ArrayAdapter<String>(DiaryActivity.this, android.R.layout.simple_list_item_1, entries);
+        listView.setAdapter(arrayAdapter);
+
+    }
+}

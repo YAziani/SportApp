3bccb0acdc3b100938373b92e351315a83d86ce3
Basti <sjestaedt@googlemail.com>
2017-01-16 20:41:40 +0100

    activate actionbar

--- app/src/main/java/com/example/mb7/sportappbp/DiaryEntryActivity.java
+++ app/src/main/java/com/example/mb7/sportappbp/DiaryEntryActivity.java
@@ -2,6 +2,7 @@ package com.example.mb7.sportappbp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;

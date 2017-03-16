8d0ada48ab52e0011344cb50e7a54161abd978ad
Basti <sjestaedt@googlemail.com>
2017-03-16 23:34:23 +0100

    add calculation of time and pints

--- app/src/main/java/com/example/mb7/sportappbp/Objects/DiaryEntry.java
+++ app/src/main/java/com/example/mb7/sportappbp/Objects/DiaryEntry.java
@@ -1,5 +1,6 @@
 package com.example.mb7.sportappbp.Objects;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 /**
@@ -8,17 +9,25 @@ import java.util.ArrayList;
 
 public class DiaryEntry {
 
+    private String id;
     private String date;
-    private Boolean successful;
+    private String time;
     private ArrayList<Exercise> exerciseList;
 
     /**
      * Constructor to create a diary entry
      * @param date : date of the training
      */
-    public DiaryEntry(String date){
+    public DiaryEntry(String id, String date, String time){
+        this.id = id;
         this.date = date;
+        this.time = time;
         exerciseList = new ArrayList<Exercise>();
+    }
+
+
+    public String getID(){
+        return this.id;
     }
 
     /**
@@ -29,13 +38,11 @@ public class DiaryEntry {
         return this.date;
     }
 
-    /**
-     * This method returns true if the training was successful, if not it returns false
-     * @return returns if the training was successful as boolean
-     */
-    public Boolean getSuccessful(){
-        return this.successful;
+
+    public String getTime(){
+        return this.time;
     }
+
 
     public int getTotalTimeMinutes() {
         int result = 0;
@@ -128,16 +135,120 @@ public class DiaryEntry {
         return result;
     }
 
-    public int getTotalTimeHoursReinerAufenthalt(){
-        int result = 0;
-        for(Exercise i : exerciseList) {
+    public String[] getTotalTimePointsAsArrayLeistungstests(){
 
-            if(i instanceof ReinerAufenthaltExercise)
-                result = result + i.getTimeHours();
+        String[] result = new String[2];
+        int resultMin = 0;
+        int resultHours = 0;
+
+        for(Exercise i : exerciseList) {
+            //calculate all "Reiner Aufenthalt" exercises
+            if(i instanceof LeistungstestsExercise) {
+                //Calculate minutes and hours
+                resultHours = resultHours + i.getTimeHours();
+                resultMin = resultMin + i.getTimeMunites();
+
+                //check, if the minutes are bigger as an hour
+                if(resultMin >= 60){
+                    resultHours ++;
+                    resultMin = resultMin % 60;
+                }
+            }
+
         }
-        return result;
+        //Calculate the points with factor 3
+        int points = (int) Math.round(((resultHours * 60) + resultMin) * 3);
+
+        //transform the time and points to a string and get it as an array back
+        return result = totalTimePointsToStringArray(resultHours, resultMin, points);
     }
 
+
+    public String[] getTotalTimePointsAsArrayTraining(){
+
+        String[] result = new String[2];
+        int resultMin = 0;
+        int resultHours = 0;
+
+        for(Exercise i : exerciseList) {
+            //calculate all "Reiner Aufenthalt" exercises
+            if(i instanceof TrainingExercise) {
+                //Calculate minutes and hours
+                resultHours = resultHours + i.getTimeHours();
+                resultMin = resultMin + i.getTimeMunites();
+
+                //check, if the minutes are bigger as an hour
+                if(resultMin >= 60){
+                    resultHours ++;
+                    resultMin = resultMin % 60;
+                }
+            }
+
+        }
+        //Calculate the points with factor 2
+        int points = (int) Math.round(((resultHours * 60) + resultMin) * 2);
+
+        //transform the time and points to a string and get it as an array back
+        return result = totalTimePointsToStringArray(resultHours, resultMin, points);
+    }
+
+
+    public String[] getTotalTimePointsAsArrayWellness(){
+
+        String[] result = new String[2];
+        int resultMin = 0;
+        int resultHours = 0;
+
+        for(Exercise i : exerciseList) {
+            //calculate all "Reiner Aufenthalt" exercises
+            if(i instanceof WellnessExercise) {
+                //Calculate minutes and hours
+                resultHours = resultHours + i.getTimeHours();
+                resultMin = resultMin + i.getTimeMunites();
+
+                //check, if the minutes are bigger as an hour
+                if(resultMin >= 60){
+                    resultHours ++;
+                    resultMin = resultMin % 60;
+                }
+            }
+
+        }
+        //Calculate the points with factor 1
+        int points = (int) Math.round(((resultHours * 60) + resultMin) * 1);
+
+        //transform the time and points to a string and get it as an array back
+        return result = totalTimePointsToStringArray(resultHours, resultMin, points);
+    }
+
+
+    public String[] getTotalTimePointsAsArrayReinerAufenthalt(){
+
+        String[] result = new String[2];
+        int resultMin = 0;
+        int resultHours = 0;
+
+        for(Exercise i : exerciseList) {
+            //calculate all "Reiner Aufenthalt" exercises
+            if(i instanceof ReinerAufenthaltExercise) {
+                //Calculate minutes and hours
+                resultHours = resultHours + i.getTimeHours();
+                resultMin = resultMin + i.getTimeMunites();
+
+                //check, if the minutes are bigger as an hour
+                if(resultMin >= 60){
+                    resultHours ++;
+                    resultMin = resultMin % 60;
+                }
+            }
+
+        }
+        //Calculate the points with factor 0.5
+        int points = (int) Math.round(((resultHours * 60) + resultMin) * 0.5);
+
+        //transform the time and points to a string and get it as an array back
+        return result = totalTimePointsToStringArray(resultHours, resultMin, points);
+    }
 
 
     public ArrayList<Exercise> getExerciseList(){
@@ -147,8 +258,8 @@ public class DiaryEntry {
     public void setDate(String date){
         this.date = date;
     }
-    public void setSuccessful(boolean successful){
-        this.successful = successful;
+    public void setTime(String time){
+        this.time = time;
     }
     public void setExerciseList(ArrayList<Exercise> newList){
         this.exerciseList = newList;
@@ -165,4 +276,21 @@ public class DiaryEntry {
             exerciseList.add(i);
         }
     }
+
+    private String[] totalTimePointsToStringArray(int hours, int min, int points){
+
+        String[] result = new String[2];
+
+        StringBuilder sbTime = new StringBuilder();
+        StringBuilder sbPoints = new StringBuilder();
+        DecimalFormat df = new DecimalFormat("00");
+        sbTime.append(df.format(hours)).append(":").append(df.format(min)).append(" h");
+        result[0] = sbTime.toString();
+
+        sbPoints.append(points).append(" Pkt.");
+        result[1] = sbPoints.toString();
+
+        return result;
+    }
+
 }
